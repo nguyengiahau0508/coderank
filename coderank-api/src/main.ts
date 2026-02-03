bfa5dd33ab68cad206be1758d415659659660f0b
Nguyen Gia Hau <nguyengiahau2004@gmail.com>
2026-02-03 19:52:04 +0700

    feat: add Swagger documentation setup and integrate @nestjs/swagger package

--- coderank-api/src/main.ts
+++ coderank-api/src/main.ts
@@ -6,11 +6,11 @@ import { AppConfigService } from './config/app/app-config.service';
 import { MariadbConfigService } from './config/db/mariadb/mariadb-config.service';
 import { COLORS } from './common/constants/colors';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['fatal','error', 'warn', 'debug', 'verbose'],
+    logger: ['fatal', 'error', 'warn', 'debug', 'verbose'],
   });
 
   app.use(cookieParser());
@@ -24,9 +24,16 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  
+
   const appConfig: AppConfigService = app.get(AppConfigService);
   const mariadbConfig: MariadbConfigService = app.get(MariadbConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .addBearerAuth()
+    .addOAuth2()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, documentFactory);
 
   await app.listen(appConfig.port, appConfig.host, () => {
     const border = `${COLORS.cyan}-----------------------------------${COLORS.reset}`;
@@ -36,13 +43,17 @@ async function bootstrap() {
     console.log(`${COLORS.bright}${COLORS.green}Environment:${COLORS.reset} ${COLORS.magenta}${appConfig.env}${COLORS.reset}`);
     console.log(`${COLORS.bright}${COLORS.green}Server running at:${COLORS.reset} ${COLORS.blue}http://${appConfig.host}:${appConfig.port}/api${COLORS.reset}`);
     console.log(border);
+    console.log(border);
+    console.log(`${COLORS.bright}${COLORS.cyan}API Documentation:${COLORS.reset}`);
+    console.log(`${COLORS.green}Swagger UI:${COLORS.reset} ${COLORS.blue}http://${appConfig.host}:${appConfig.port}/api-docs${COLORS.reset}`);
+    console.log(border + '\n');
     console.log(`${COLORS.bright}${COLORS.cyan}Database Connection:${COLORS.reset}`);
     console.log(`${COLORS.green}Type:${COLORS.reset} ${COLORS.yellow}MariaDB${COLORS.reset}`);
     console.log(`${COLORS.green}Host:${COLORS.reset} ${COLORS.yellow}${mariadbConfig.host}${COLORS.reset}`);
     console.log(`${COLORS.green}Port:${COLORS.reset} ${COLORS.yellow}${mariadbConfig.port}${COLORS.reset}`);
     console.log(`${COLORS.green}Database:${COLORS.reset} ${COLORS.yellow}${mariadbConfig.dbName}${COLORS.reset}`);
     console.log(`${COLORS.green}Username:${COLORS.reset} ${COLORS.yellow}${mariadbConfig.username}${COLORS.reset}`);
-    console.log(border + '\n');
+
   });
 }
 bootstrap();
