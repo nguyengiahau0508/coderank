f61d4f28ea9da0a66ead75c2f69667fa85e19520
Nguyen Gia Hau <nguyengiahau2004@gmail.com>
2026-02-04 18:14:00 +0700

    feat(runner): add Runner module, controller, and service for new functionality

--- coderank-api/src/module/module.ts
+++ coderank-api/src/module/module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "./user/user.module";
+import { RunnerModule } from "./runner/runner.module";
 
 
 @Module({
     imports: [
-        UserModule
+        UserModule,
+        RunnerModule,
     ],
 })
 export class RootModule {}
