45df53cd7a17a5b4dbc7e9b85f27237be05a20f8
Nguyen Gia Hau <nguyengiahau2004@gmail.com>
2026-03-29 22:56:01 +0700

    update project

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,18 +13,6 @@ module.exports = {
       },
     },
     {
-      name: 'coderank-client',
-      cwd: './coderank-client',
-      script: 'npm',
-      args: 'run pm2:start',
-      env_development: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-    },
-    {
       name: 'coderank-agent',
       cwd: './coderank-agent',
       script: 'npm',
